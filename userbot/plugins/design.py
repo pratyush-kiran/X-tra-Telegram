9197b03a8720a6adf321f5d334e7b32522d08058
pratyush-kiran <65016876+pratyush-kiran@users.noreply.github.com>
2020-05-24 22:29:23 +0530

    Update design.py

--- userbot/plugins/design.py
+++ userbot/plugins/design.py
@@ -36,3 +36,36 @@ async def _(event):
     else:
         await event.reply(mentions)
     await event.delete()    
+
+    
+    @borg.on(admin_cmd("uc"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "PUBG UC Prices\n\n8100 UC - Rs 3500 ($ 50)\n16000uc - Rs 6000 ($ 80)\n24000 - Rs 9000 ($ 120)"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete() 
+    
+    @borg.on(admin_cmd("pay_meth"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "the following Payment methods are accepted:\n\nPhone Pe\nGoogle Pay\nPaytm(KYC completed)\nBitcoins\nPayPal\nSkrill"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete() 
