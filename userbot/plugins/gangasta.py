f3439edd80c876209817fa5dd56704e9fab67934
pratyush-kiran <65016876+pratyush-kiran@users.noreply.github.com>
2020-05-25 04:13:34 +0530

    Update gangasta.py

--- userbot/plugins/gangasta.py
+++ userbot/plugins/gangasta.py
@@ -5,21 +5,21 @@ import asyncio
 
 
 
-@borg.on(admin_cmd("gangasta ?(.*)"))
+@borg.on(admin_cmd("gangster ?(.*)"))
 async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         await event.edit("EVERyBOdy")
-        await asyncio.sleep(0.3)
+        await asyncio.sleep(1)
         await event.edit("iZ")
-        await asyncio.sleep(0.2)
+        await asyncio.sleep(1)
         await event.edit("GangSTur")
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(1)
         await event.edit("UNtIL ")
-        await asyncio.sleep(0.2)
+        await asyncio.sleep(1)
         await event.edit("I")
-        await asyncio.sleep(0.3)
+        await asyncio.sleep(1)
         await event.edit("ArRivE")
-        await asyncio.sleep(0.3)
+        await asyncio.sleep(1)
         await event.edit("🔥🔥🔥")
-        await asyncio.sleep(0.3)
+        await asyncio.sleep(1)
         await event.edit("EVERyBOdy iZ GangSTur UNtIL I ArRivE 🔥🔥🔥")
