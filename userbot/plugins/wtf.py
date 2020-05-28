9080848eac17fc54910215b63815a945d9a814d9
pratyush-kiran <65016876+pratyush-kiran@users.noreply.github.com>
2020-05-28 22:11:22 +0530

    Update wtf.py

--- userbot/plugins/wtf.py
+++ userbot/plugins/wtf.py
@@ -41,33 +41,3 @@ async def _(event):
             await asyncio.sleep(animation_interval)
             await event.edit(animation_chars[i])
 
-@borg.on(admin_cmd("(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    animation_interval = 0.5
-    animation_ttl = range(0, 14)
-    input_str = event.pattern_match.group(1)
-    if input_str == "pay2":
-        await event.edit(input_str)
-        animation_chars = [
-            "`8455015107indus@indus`👈 \n👆 Click here to Copy👆 \n\nPay on this UPI ID through Phone Pe/Google Pay",
-            "`8455015107indus@indus` 👈\n 👆__Click here to Copy__ 👆\n\n**Pay** on this UPI ID through Phone Pe/Google Pay",
-            "`8455015107indus@indus`👈 \n👆 Click here to Copy👆 \n\nPay on this UPI ID through Phone Pe/Google Pay",
-            "`8455015107indus@indus` 👈\n 👆__Click here to Copy__ 👆\n\n**Pay** on this UPI ID through Phone Pe/Google Pay",
-            "`8455015107indus@indus`👈 \n👆 Click here to Copy👆 \n\nPay on this UPI ID through Phone Pe/Google Pay",
-            "`8455015107indus@indus` 👈\n 👆__Click here to Copy__ 👆\n\n**Pay** on this UPI ID through Phone Pe/Google Pay",
-            "`8455015107indus@indus`👈 \n👆 Click here to Copy👆 \n\nPay on this UPI ID through Phone Pe/Google Pay",
-            "`8455015107indus@indus` 👈\n 👆__Click here to Copy__ 👆\n\n**Pay** on this UPI ID through Phone Pe/Google Pay",
-            "`8455015107indus@indus`👈 \n👆 Click here to Copy👆 \n\nPay on this UPI ID through Phone Pe/Google Pay",
-            "`8455015107indus@indus` 👈\n 👆__Click here to Copy__ 👆\n\n**Pay** on this UPI ID through Phone Pe/Google Pay",
-            "`8455015107indus@indus`👈 \n👆 Click here to Copy👆 \n\nPay on this UPI ID through Phone Pe/Google Pay",
-            "`8455015107indus@indus` 👈\n 👆__Click here to Copy__ 👆\n\n**Pay** on this UPI ID through Phone Pe/Google Pay",
-            "`8455015107indus@indus`👈 \n👆 Click here to Copy👆 \n\nPay on this UPI ID through Phone Pe/Google Pay",
-            "`8455015107indus@indus` 👈\n 👆__Click here to Copy__ 👆\n\n**Pay** on this UPI ID through Phone Pe/Google Pay"
-]
-
-        for i in animation_ttl:
-        	
-            await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i])
